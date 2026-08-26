cdc8a733473a265fd3afe38ff728f418551d1896
Alexander <alexander@makklays.com.ua>
2026-08-26 22:05:21 +0200

    26-08-2026 Add new 'User Wallet' Vertical Slices for project 'EV Charging network'.

--- src/main/resources/db/migration/V3__create_user_wallets_table.sql
+++ src/main/resources/db/migration/V3__create_user_wallets_table.sql
@@ -4,13 +4,14 @@
 -- Creating table 'user_wallets' for storing main token data
 -- Таблица кошельков пользователей сети EV Charging network с поддержкой оптимистической блокировки
 CREATE TABLE IF NOT EXISTS user_wallets (
-    user_id UUID PRIMARY KEY,
-    balance NUMERIC(12,4) NOT NULL DEFAULT 0.0000,
-    version BIGINT NOT NULL DEFAULT 0, -- Версия записи для паттерна Optimistic Locking (предотвращение Race Conditions)
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id          UUID PRIMARY KEY,
+    user_id     BIGINT NOT NULL REFERENCES users(id),
+    balance     NUMERIC(12,4) NOT NULL DEFAULT 0.0000,
+    version     BIGINT NOT NULL DEFAULT 0, -- Версия записи для паттерна Optimistic Locking (предотвращение Race Conditions)
+    created_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Индекс для оптимизации проверок баланса и частых апдейтов
-CREATE INDEX idx_user_wallets_balance ON user_wallets (user_id, balance);
+CREATE INDEX IF NOT EXISTS idx_user_wallets_user_id ON user_wallets(user_id);
 
