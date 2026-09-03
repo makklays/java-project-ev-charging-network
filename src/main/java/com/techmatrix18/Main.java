ed35834571c38f66951359f95a56ca9ffd531b52
Alexander <alexander@makklays.com.ua>
2026-09-03 22:13:38 +0200

    03-09-2026 Add new Main file for project 'EV Charging network'.

--- src/main/java/com/techmatrix18/Main.java
+++ src/main/java/com/techmatrix18/Main.java
@@ -1,7 +1,13 @@
 package com.techmatrix18;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Main
+ * Главная точка входа и запуска распределенной платформы EV Charging Network.
+ * Активирует автоконфигурацию Spring Boot и фоновые шедулеры Outbox-паблишера.
  *
  * @author Alexander Kuziv <makklays@gmail.com>
  * @company TechMatrix18
@@ -9,9 +15,14 @@ package com.techmatrix18;
  * @since 17.08.2026
  */
 
+@SpringBootApplication
+@EnableScheduling
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello EV Charging network world!");
+
+        // Start Spring Boot
+        SpringApplication.run(Main.class, args);
     }
 }
 
